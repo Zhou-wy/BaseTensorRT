a6f62b8d3bdaf4c6c65cc6e44de31a9cab45587d
Zwyywz <2867283770@qq.com>
2022-10-11 15:55:27 +0800

    onnx editor

---
+++ 3.3-onnx-editor/edit-onnx.py
@@ -0,0 +1,24 @@
+'''
+Descripttion: 
+version: 
+Author: zwy
+Date: 2022-09-30 11:06:00
+LastEditors: zwy
+LastEditTime: 2022-10-01 11:25:30
+'''
+import math
+import torch
+import os
+import onnx
+import numpy as np
+
+
+model = onnx.load_model("my.onnx")
+
+conv_weight = model.graph.initializer[0]
+conv_bias = model.graph.initializer[1]
+
+conv_weight.raw_data = np.arange(9, dtype=np.float32).tobytes()
+
+onnx.save_model(model, "demo.change.onnx")
+
